843a91a2849a4ec3f4df518125f9fa04e45e8d3d
dealen <marcickiewiczjakub@gmail.com>
2024-03-07 22:19:27 +0100

    Added method in client app for real api testing purposes.

--- rusty_open_ai_client/src/main.rs
+++ rusty_open_ai_client/src/main.rs
@@ -1,8 +1,24 @@
 use rusty_open_ai_lib::open_ai::OpenAi;
 
-fn main() {
+#[tokio::main]
+async fn main() {
 
-    let open_ai = OpenAi::new("key".to_string(), "model".to_string());
+    let open_ai = OpenAi::new("key".to_string(), "gpt-3.5-turbo-0125".to_string());
+
+    show_available_models(open_ai).await;
 
     println!("Hello, world!");
+}
+
+async fn show_available_models(open_ai: OpenAi) {
+    match open_ai.get_model_list().await {
+        Ok(list) => {
+            for model in list.data {
+                println!("Model: {:?}", model);
+            }
+        }
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
 }
